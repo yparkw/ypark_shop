54940dacea9400cd916bef607b3699700cdc3912
Lee <ehgus8621@gmail.com>
2023-12-07 00:49:59 +0900

    backend 환경설정 완료, Phillow, mysqlclient pip upgrade 후 설치

---
+++ frontend_react/src/components/Commons/SignInput.jsx
@@ -0,0 +1,60 @@
+/* eslint-disable react/prop-types */
+import React, { memo, useState } from "react";
+import styled from "styled-components";
+
+export default memo(function SignInput(props) {
+  const [check, setCheck] = useState(true);
+
+  const checkValidate = (value) => {
+    const isValidate = props.onBlur(value);
+    props.setValid(isValidate);
+    setCheck(isValidate);
+  };
+
+  return (
+    <Container>
+      <label htmlFor={props.name}>{props.label}</label>
+      <InputWrapper>
+        <input
+          type={props.type || "text"}
+          id={props.name}
+          placeholder={props.text}
+          name={props.name}
+          onChange={(e) => props.changeHandler(e)}
+          onKeyUp={() => checkValidate(props.value)}
+          value={props.value}
+        />
+      </InputWrapper>
+      {!check && <ErrorMassage>{props.errorMassage}</ErrorMassage>}
+    </Container>
+  );
+});
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  label {
+    font-weight: 600;
+  }
+`;
+
+const InputWrapper = styled.div`
+  border-bottom: 1px solid #9e9e9e;
+
+  input {
+    width: 100%;
+    font-size: 1rem;
+    margin-bottom: 8px;
+    border: none;
+    outline: none;
+    line-height: 1.2rem;
+  }
+`;
+
+const ErrorMassage = styled.span`
+  color: #eb5252;
+  font-size: 14px;
+`;
---
+++ frontend_react/src/hooks/useGetProductDetailInfo.jsx
@@ -0,0 +1,22 @@
+import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
+import { axiosInstance } from "../api/axiosInstance";
+
+export const getProductDetailInfo = async (params) => {
+  const res = await axiosInstance.get(`/api/v2/products/?name=${params.name}`);
+  return res.data.data;
+};
+
+export default function useGetProductDetailInfo(params) {
+  const navigate = useNavigate();
+  const { data, isLoading } = useQuery(
+    ["getProductDetailInfo", params],
+    () => getProductDetailInfo(params),
+    {
+      retry: 1,
+      onError: () => navigate("/*"),
+    }
+  );
+
+  return { data, isLoading };
+}
