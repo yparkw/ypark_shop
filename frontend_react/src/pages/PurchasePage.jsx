b4aacee46864935a78222b84ffac9bf680e572de
Lee <ehgus8621@gmail.com>
2024-01-17 12:06:33 +0900

    구매할 것들 가져와야 함

--- frontend_react/src/pages/PurchasePage.jsx
+++ frontend_react/src/pages/PurchasePage.jsx
@@ -1,21 +1,21 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 import PurchaseForm from "../components/Purchase/PurchaseForm";
 
 
 export default function PurchasePage() {
-    // const userInfo = useSelector((state) => state.user.info);
+    const userInfo = useSelector((state) => state.user);
 
-    const userInfo = {
-        address: '1234 Main St',
-        phone: '123-456-7890',
-        email: 'user@example.com',
-        postcode: '10001',
-        // ... more user info
-      }; // 임시 유저
+    // const userInfo = {
+    //     address: '1234 Main St',
+    //     phone: '123-456-7890',
+    //     email: 'user@example.com',
+    //     postcode: '10001',
+    //     // ... more user info
+    //   }; // 임시 유저
     const cartItems = [
         { name: 'Product 1', quantity: 1, price: '1000' },
         { name: 'Product 2', quantity: 2, price: '2000' },
