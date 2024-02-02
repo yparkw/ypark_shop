7f7bc2c5696c05f72afa9e8d628235feabae4247
Lee <ehgus8621@gmail.com>
2024-02-02 17:13:37 +0900

    why:

--- server/config/settings/prod.py
+++ server/config/settings/prod.py
@@ -14,6 +14,7 @@ CORS_ALLOW_ALL_ORIGINS = False # 개발용, 실제 배포시 보안을 위해 �
 CORS_ALLOWED_ORIGINS = [
     "http://www.yparkw.com",
     "https://www.yparkw.com",    # 실제 배포된 React 앱의 도메인
+    "http://211.45.167.63:3000",
     "https://211.45.167.63:3000",
 ]
 CORS_ALLOW_CREDENTIALS = True # 쿠키와 함꼐 요청을 보낼 수있도록
