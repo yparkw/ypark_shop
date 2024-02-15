0d6a0761321af922c2b5f01cc0f9ab6b9379bd40
Lee <ehgus8621@gmail.com>
2024-02-15 18:27:49 +0900

    re

--- server/config/settings/prod.py
+++ server/config/settings/prod.py
@@ -9,6 +9,7 @@ STATIC_ROOT = '/app/static/'
 STATICFILES_DIRS = []
 
 MEDIA_ROOT = '/app/media'
+MEDIA_URL = 'https://www.yparkw.com/media/'
 
 CORS_ALLOW_ALL_ORIGINS = False # 개발용, 실제 배포시 보안을 위해 수정 필요
 CORS_ALLOWED_ORIGINS = [
