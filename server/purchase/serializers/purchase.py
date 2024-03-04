ee3fd328c11836cef1c528163f1424ac6dd93a4c
Lee <ehgus8621@gmail.com>
2024-03-04 21:20:42 +0900

    POST purchase/verify_purchase/: success, after do selenium

--- server/purchase/serializers/purchase.py
+++ server/purchase/serializers/purchase.py
@@ -1,24 +1,29 @@
 from rest_framework import serializers
 from purchase.models.purchase import Purchase, PurchaseItem
 
+import logging
+logger = logging.getLogger(__name__)
+
 class PurchaseItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = PurchaseItem
         fields = '__all__'
 
 class PurchaseSerializer(serializers.ModelSerializer):
-    items = PurchaseItemSerializer(many=True, read_only=True)
+    products = PurchaseItemSerializer(many=True)
 
     class Meta:
         model = Purchase
         fields = [
             'id', 'imp_uid', 'merchant_uid', 'amount', 'status', 
             'buyer_name', 'buyer_email', 'buyer_tel', 'buyer_address', 
-            'buyer_detailAddress', 'buyer_postcode', 'items'
+            'buyer_detailAddress', 'buyer_postcode', 'products'
         ]
         
     def create(self, validated_data):
-        items_data = validated_data.pop('items')
+        logger.debug(validated_data)
+        items_data = validated_data.pop('products')
+        
         purchase = Purchase.objects.create(**validated_data)
         for item_data in items_data:
             PurchaseItem.objects.create(purchase=purchase, **item_data)
