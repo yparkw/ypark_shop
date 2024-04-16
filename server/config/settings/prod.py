829aaa496ccfbf316c7932998a23a6b9c611929e
Lee <ehgus8621@gmail.com>
2024-04-16 18:49:46 +0900

    chore: change ip
    cafe24에서 google cloud로 서버를 옮김 그에 따라 도메인이 사라지고 ip주소도 변경되어 그 사항을 변경함

--- server/config/settings/prod.py
+++ server/config/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 Debug = False
 
 ALLOWED_HOSTS = ['api.ipify.org', 'www.shadowserver.org', 'ip-api.com', 'google.com',
-                 '211.45.167.63', 'yparkw.com', 'www.yparkw.com', 'api64.ipify.org']
+                 '34.125.116.9', 'yparkw.com', 'www.yparkw.com', 'api64.ipify.org']
 
 STATIC_ROOT = '/app/static/'
 STATICFILES_DIRS = []
@@ -18,8 +18,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://yparkw.com",
     "https://yparkw.com",
     "https://www.yparkw.com",    # 실제 배포된 React 앱의 도메인
-    "http://211.45.167.63:3000",
-    "https://211.45.167.63:3000",
+    "http://34.125.116.9:3000",
+    "https://34.125.116.9:3000",
 ]
 CORS_ALLOW_CREDENTIALS = True # 쿠키와 함꼐 요청을 보낼 수있도록
 
