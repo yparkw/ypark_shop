1ec49716f8cd4fd6b4a23786cdb1908bb8cce29e
Lee <ehgus8621@gmail.com>
2024-04-22 20:36:15 +0900

    build: face 400 error
    지금 docker-compose 빌드는 잘됨 nginx파일이 문제 이거나 client 소스코드가 문제인듯 400 에러 해결 실패

--- server/config/settings/local.py
+++ server/config/settings/local.py
@@ -20,7 +20,41 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True # 쿠키와 함꼐 요청을 보낼 수있도록
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'yparkw',
+        'USER': 'admin',
+        'PASSWORD': 'qkr102998!!',
+        'HOST': 'db',  # 혹은 도커 서비스 이름
+        'PORT': 5432,
+    }
+}
+
 
 
 IAMPORT_KEY = '1513772424248766',
 IAMPORT_SECRET = 'jCgJnrqDO8WS3XXpIGMB5Gd8KtjdNlx9TRKFw7tGcuuI70QxdaKlpHXu7W8dypdklSQ2mk1vczmxquCe',
+
+AUTH_PASSWORD_VALIDATORS = [
+    # 기존의 비밀번호 검증기 설정들...
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 9,  # 원하는 최소 비밀번호 길이 설정
+        }
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+    # 여기에 커스텀 비밀번호 검증기 추가
+    {
+        'NAME': 'user.validators.CustomPasswordValidator',  # 'your_app_name'을 앱의 실제 이름으로 변경
+    },
+]
