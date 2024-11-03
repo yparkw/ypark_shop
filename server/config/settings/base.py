4530961a573a69608d48cdf9307b19446469c7d8
Lee <ehgus8621@gmail.com>
2024-11-03 23:11:29 +0900

    ci-cd: sentry-dsn is changed

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -289,7 +289,7 @@ AUTH_PASSWORD_VALIDATORS = [
 import sentry_sdk
 
 sentry_sdk.init(
-    dsn=env('sentry_dsn'),
+    dsn=env('SENTRY_DSN'),
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for tracing.
     traces_sample_rate=1.0,
